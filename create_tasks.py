1c93c563318e2d628665d5ba02ee13128f040d1c
Vincent Lordier <vlordier@users.noreply.github.com>
2024-01-17 16:08:46 +0100

    update README.md

--- create_tasks.py
+++ create_tasks.py
@@ -1,19 +1,20 @@
-import instructor
-from openai import OpenAI
-
-from pydantic import BaseModel
 import logging
-import os 
-from dotenv import load_dotenv
+import os
 
+import instructor
 import yaml
-import config 
+from dotenv import load_dotenv
+from openai import OpenAI
+from pydantic import BaseModel
+
+import config
 
 load_dotenv()
 
 # set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 class Agent(BaseModel):
     role: str
@@ -22,16 +23,23 @@ class Agent(BaseModel):
     verbose: bool
     allow_delegation: bool
     tools: list
-    agent_name: str   
+    agent_name: str
+
 
 class Task(BaseModel):
     description: str
     agent: Agent
 
+
+class TasksList(BaseModel):
+    tasks: list
+
+
 # setup OpenAI client
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 # patch OpenAI client with instructor
 client = instructor.patch(client)
+
 
 def get_model_name():
     if not config.OPENAI_MODEL_TASKS:
@@ -41,9 +49,12 @@ def get_model_name():
         if model_name.id == config.OPENAI_MODEL_TASKS:
             print(f"Selected model for tasks definition: {model_name.id}")
             return config.OPENAI_MODEL_TASKS
-    raise ValueError(f"{config.OPENAI_MODEL_TASKS} is not a valid model. Valid models are: {model_lst}")
+    raise ValueError(
+        f"{config.OPENAI_MODEL_TASKS} is not a valid model. Valid models are: {model_lst}"
+    )
 
-# open agents.yaml to ensure it is valid yaml
+
+# open agents.yaml to ensure it is valid yaml
 with open("agents.yaml") as file:
     agents_data = yaml.safe_load(file)
     agents_description = yaml.dump(agents_data)
@@ -55,7 +66,9 @@ task_description = " We want to define which micro tasks of the gig economy can 
 
 model = get_model_name()
 
-prompt_define_tasks = config.PROMPT_DEFINE_TASKS.format(task_description=task_description, agents_description=agents_description)
+prompt_define_tasks = config.PROMPT_DEFINE_TASKS.format(
+    task_description=task_description, agents_description=agents_description
+)
 system_define_tasks = config.SYSTEM_PROMPT_DEFINE_TASKS
 messages = [
     {"role": "system", "content": system_define_tasks},
@@ -67,11 +80,9 @@ completion = client.chat.completions.create(
     messages=messages,
     max_tokens=4096,
     n=1,
-    response_model=Task, 
+    response_model=TasksList,
     response_format={"type": "text"},
     stop=None,
 )
 
 print(completion)
-
-
